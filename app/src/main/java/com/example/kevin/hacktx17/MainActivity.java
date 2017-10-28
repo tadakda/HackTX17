27a109d09d9377b93f0b89ba028a699665e0df7f
Kevin Dao <kevindao2006@gmail.com>
2017-10-28 17:18:28 -0500

    graph

--- app/src/main/java/com/example/kevin/hacktx17/MainActivity.java
+++ app/src/main/java/com/example/kevin/hacktx17/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -20,7 +22,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LineChart chart = (LineChart) findViewById(R.id.chart);
+        chart.setTouchEnabled(false);
         chart.setDrawGridBackground(false);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setDrawGridLines(false);
+
+        chart.getAxisRight().setDrawAxisLine(false);
+        chart.getAxisRight().setDrawGridLines(false);
+        chart.getAxisRight().setDrawLabels(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+
         List<Entry> entries = new ArrayList<Entry>();
 
         entries.add(new Entry(5, 6));
