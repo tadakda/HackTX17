9b1b275fa4bb7bfd0a4288773a6788244b06d043
Kevin Dao <kevindao2006@gmail.com>
2017-10-28 16:53:37 -0500

    graph

--- app/src/main/java/com/example/kevin/hacktx17/MainActivity.java
+++ app/src/main/java/com/example/kevin/hacktx17/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.kevin.hacktx17;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+        chart.setDrawGridBackground(false);
+        List<Entry> entries = new ArrayList<Entry>();
+
+        entries.add(new Entry(5, 6));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        dataSet.setColor(Color.BLACK);
+        dataSet.setValueTextColor(Color.BLACK);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+
     }
 }
